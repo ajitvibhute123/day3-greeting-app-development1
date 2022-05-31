6dc2c62a9b297de1c1282ba0718917e777810ee9
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-06-01 02:23:58 +0530

    Added:Finding greeting message

--- src/main/java/com/bz/greetingappdevelopment/Services/GreetingServices.java
+++ src/main/java/com/bz/greetingappdevelopment/Services/GreetingServices.java
@@ -3,15 +3,21 @@ package com.bz.greetingappdevelopment.Services;
 import com.bz.greetingappdevelopment.controller.Greeting;
 import com.bz.greetingappdevelopment.dto.User;
 import com.bz.greetingappdevelopment.dto.UserDto;
+import com.bz.greetingappdevelopment.repository.IGreetingRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class GreetingServices implements IGreetingService {
     private static final String template = "Hello world";
     private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    IGreetingRepository iGreetingRepository;
 
     @Override
     public Greeting greetingMessage() {
@@ -25,5 +31,9 @@ public class GreetingServices implements IGreetingService {
         modelMapper.map(userDto, user);
         return ("Hello " + user.getFirstName() + " " + user.getLastName());
     }
-
+    @Override
+    public User getById(long id) {
+        Optional<User> greetById = iGreetingRepository.findById(id);
+        return greetById.orElse(null);
+    }
 }
