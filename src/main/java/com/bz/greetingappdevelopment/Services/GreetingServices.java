153bdde2a12d537e6dbdf6ccb54119559f20331c
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-06-01 02:13:50 +0530

    Added:Greeting message

--- src/main/java/com/bz/greetingappdevelopment/Services/GreetingServices.java
+++ src/main/java/com/bz/greetingappdevelopment/Services/GreetingServices.java
@@ -1,6 +1,9 @@
 package com.bz.greetingappdevelopment.Services;
 
 import com.bz.greetingappdevelopment.controller.Greeting;
+import com.bz.greetingappdevelopment.dto.User;
+import com.bz.greetingappdevelopment.dto.UserDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -15,4 +18,13 @@ public class GreetingServices implements IGreetingService {
     public Greeting greetingMessage() {
         return new Greeting(counter.incrementAndGet(), String.format(template));
     }
+
+    @Override
+    public String greetingMessageByName(UserDto userDto) {
+        User user = new User();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.map(userDto, user);
+        return ("Hello " + user.getFirstName() + " " + user.getLastName());
+    }
+
 }
